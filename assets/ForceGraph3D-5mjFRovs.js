65902b85943df6797884cd0606a0a4c00851a149
hdrengnr <as@hydra.engineer>
2025-12-15 22:32:31 +0400

    chore: hide THEME/LANGUAGE/VIEW selectors for production
    Production cleanup:
    - THEME selector: hidden for site modes
    - LANGUAGE selector: hidden for site modes
    - VIEW selector: hidden for site modes
    
    Mobile burger menu now only shows Navigation items for gorizont.ai sections.
    
    Technical:
    - Wrapped selectors in {!isSiteHeader && (<>...</>)} condition
    - Updated build (ControlPanel.jsx changes)

--- assets/ForceGraph3D-5mjFRovs.js
+++ assets/ForceGraph3D-5mjFRovs.js
@@ -1,4 +1,4 @@
-import{f as vE,h as bo,i as _E,a as ky,r as zt,j as ve}from"./index-DI-9SPYX.js";import{_ as Yl}from"./extends-CF3RwP-h.js";var rp={exports:{}},Ns={};/**
+import{f as vE,h as bo,i as _E,a as ky,r as zt,j as ve}from"./index-BSUL8Hu_.js";import{_ as Yl}from"./extends-CF3RwP-h.js";var rp={exports:{}},Ns={};/**
  * @license React
  * react-reconciler-constants.production.js
  *
